c20bf744a7155857147696e3e834115360d48a56
aniketfuryrocks <contact@aniketprajapati.me>
2022-12-09 10:16:03 +0530

    a

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use std::time::Duration;
-
 use lite_rpc_tests::client::LiteClient;
 use lite_rpc_tests::{create_transaction, new_funded_payer};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::native_token::LAMPORTS_PER_SOL;
 use solana_sdk::signature::Keypair;
 use solana_sdk::transaction::Transaction;
-use tokio::time::sleep;
 
 const RPC_ADDR: &str = "http://127.0.0.1:8890";
 //const RPC_ADDR: &str = "http://127.0.0.1:8899";
@@ -17,6 +14,7 @@ const NUM_OF_TXS: usize = 100;
 async fn main() {
     let lite_client = LiteClient(RpcClient::new(RPC_ADDR.to_string()));
     let funded_payer = new_funded_payer(&lite_client, LAMPORTS_PER_SOL * 2).await;
+
     println!("payer {}", funded_payer.to_base58_string());
 
     let txs = generate_txs(&funded_payer, &lite_client.0).await;
@@ -24,7 +22,6 @@ async fn main() {
     println!("sending tx(s)");
 
     for tx in txs {
-        sleep(Duration::from_millis(800)).await;
         lite_client.send_transaction(&tx).await.unwrap();
         println!("tx {}", &tx.signatures[0]);
     }
